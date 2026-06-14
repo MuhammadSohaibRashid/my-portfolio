0b245bd0daee9439de951b88a9b80a2da7667f4d
Muhammad Sohaib Rashid <sohaibrashid678@gmail.com>
2026-06-14 20:59:18 +0500

    Update Deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {
   FaLinkedin, FaGithub, FaEnvelope, FaMobile,
-  FaPython, FaDatabase, FaAws, FaChartBar,
-  FaCode, FaRobot, FaGraduationCap, FaChartLine,
+    FaGraduationCap, FaChartLine,
   FaExternalLinkAlt, FaChevronDown, FaChevronUp
 } from 'react-icons/fa';
 import './App.css';
